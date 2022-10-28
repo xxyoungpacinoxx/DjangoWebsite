6cb5b56de9f889d8dc26d7ba7a599f16278e20a6
Matin Zareinik <matinzareinik@gmail.com>
2022-10-28 18:42:05 +0330

    Create Account Profile Complete

--- PD/accounts/views.py
+++ PD/accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class CreateProfile(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/register.html'
+
